2ee2b23d8cac7d7e77054d3e1cfdf37229ccbdbe
Peter Buchegger <peter@pbuchegger.at>
2023-01-04 11:46:30 +0100

    set output variables to sensitive

--- hetzner/output.tf
+++ hetzner/output.tf
@@ -2,20 +2,24 @@ output "swarm-manager" {
   value = tomap({
     (hcloud_server.swarm-manager.name) = hcloud_server.swarm-manager.ipv4_address
   })
+  sensitive = true
 }
 
 output "swarm-database" {
   value = tomap({
     (hcloud_server.swarm-database.name) = hcloud_server.swarm-database.ipv4_address
   })
+  sensitive = true
 }
 
 output "swarm-workers" {
   value = zipmap(
     hcloud_server.swarm-worker.*.name, hcloud_server.swarm-worker.*.ipv4_address
   )
+  sensitive = true
 }
 
 output "ssh-public-key" {
   value = data.hcloud_ssh_key.deploy.public_key
+  sensitive = true
 }
